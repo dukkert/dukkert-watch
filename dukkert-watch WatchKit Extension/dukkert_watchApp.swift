3af6937079e9f82a34b2c9689d5945d919e5a669
itschip <onlygamernerd@gmail.com>
2022-06-06 22:53:34 +0200

    Initial Commit

---
+++ dukkert-watch WatchKit Extension/dukkert_watchApp.swift
@@ -0,0 +1,19 @@
+//
+//  dukkert_watchApp.swift
+//  dukkert-watch WatchKit Extension
+//
+//  Created by Christopher Gjelten on 06/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct dukkert_watchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
